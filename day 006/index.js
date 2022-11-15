492f58cc2b0f428f267ba408730e738cc6edfb89
jason@karjadi.com <jason@karjadi.com>
2022-11-15 09:15:53 +0700

    day 6 beforehand again

--- day 006/index.js
+++ day 006/index.js
@@ -17,23 +17,38 @@
 // Average
 
 class Student {
-  name = "";
-  email = "";
-  age = new Date();
-  score = 0;
+  constructor(newName, newEmail, newAge, newScore) {
+    this.name = typeof newName === "string" ? newName : "";
+    this.email = typeof newEmail === "string" ? newEmail : "";
+    this.age = typeof newAge === "object" ? newAge : "";
+    this.score = typeof newScore === "number" ? newScore : "";
+  }
 }
+let studentsData = [
+  new Student("Abby", "abby@gmail.com", new Date("2000-11-12"), 70),
+  new Student("Katrina", "katrina@gmail.com", new Date("2000-10-12"), 65),
+  new Student("Inigo", "inigo@gmail.com", new Date("2001-11-11"), 30),
+];
 
-let studentData = [{ name: "Abby", email: "abby@" }];
 const findLim = (numArr = [0], findHigh = true) =>
   numArr.reduce((prevVal, currVal) =>
     (findHigh ? prevVal > currVal : prevVal < currVal) ? prevVal : currVal
   );
-const createArr = (arr = [{}], key = "") => arr.map((val) => val[key]);
 const findAvg = (numArr) =>
   numArr.reduce((prevVal, currVal) => prevVal + currVal) / numArr.length;
+
 const surveyScoreAge = (studentArr = [new Student()]) => {
-  let ageArr = createArr(studentArr, "age");
-  let scoreArr = createArr(studentArr, "score");
+  let currTime = new Date();
+
+  let ageArr = studentArr.map(({ age }) => {
+    const getDiffInDate = (key = "") => currTime[key]() - age[key]();
+    let keys = ["getFullYear", "getMonth", "getDate"];
+    let timeDiffs = keys.map((key) => getDiffInDate(key));
+    (timeDiffs[1] < 0 || (timeDiffs[1] == 0 && timeDiffs[2] < 0)) &&
+      --timeDiffs[0];
+    return timeDiffs[0];
+  });
+  let scoreArr = studentArr.map(({ score }) => score);
 
   let highScore = findLim(scoreArr, true);
   let lowScore = findLim(scoreArr, false);
@@ -48,7 +63,7 @@ const surveyScoreAge = (studentArr = [new Student()]) => {
     age: { highest: highAge, lowest: lowAge, average: avgAge },
   };
 };
-let surveyData = surveyScoreAge();
+let surveyData = surveyScoreAge(studentsData);
 console.log(surveyData);
 
 // Create a program to create transaction
@@ -65,3 +80,17 @@ console.log(surveyData);
 // Add to cart method → Add product to transaction
 // Show total method → Show total current transaction
 // Checkout method → Finalize transaction, return transaction data
+
+class Product {
+  constructor() {
+    this._name = "";
+    this._price = 0;
+  }
+}
+
+class Transaction extends Product {
+  constructor() {
+    super();
+    this._total = 0;
+  }
+}
